3ba088348d854d711bd7723e5e496fdb1765aed2
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-07-27 11:14:17 -0400

    create redux branch

--- react-native-redux/App.js
+++ react-native-redux/App.js
@@ -1,5 +1,6 @@
 import "react-native-gesture-handler"; // <= DO NOT REMOVE or DO NOT MOVE DOWN - DO NOT TOUCH!
 import * as React from "react";
+// This is redux example
 
 import { Provider } from "react-redux";
 import { reduxStore } from "./auth_redux/store";
